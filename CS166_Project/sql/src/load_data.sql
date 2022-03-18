fa9afc085a825f368e8d55675fba052f8b1ca42e
Yanjun Guan <67297489+yanjun934523@users.noreply.github.com>
2022-03-18 07:31:02 -0700

    Add files via upload

---
+++ CS166_Project/sql/src/load_data.sql
@@ -0,0 +1,45 @@
+COPY USR (
+	userId,
+	passwor,
+	email,
+	name,
+	dateOfBirth)
+FROM 'USR.csv'
+WITH DELIMITER ';';
+
+COPY WORK_EXPR(
+	userId,
+	company,
+	role,
+	location,
+	startDate,
+	endDate)
+FROM 'WORK_EXPR.csv'
+WITH DELIMITER ',';
+
+COPY EDUCATIONAL_DETAILS (
+	userId,
+	instituitionName,
+	major,
+	degree,
+	startdate,
+	enddate)
+FROM 'EDUCATIONAL_DETAILS.csv'
+WITH DELIMITER ',';
+
+COPY MESSAGE (
+	msgId,
+	senderId,
+	receiverId,
+	contents,
+	deleteStatus,
+	status)
+FROM 'MESSAGE.csv'
+WITH DELIMITER ',';
+
+COPY CONNECTION_USR (
+	userId,
+	connectionId,
+	status)
+FROM 'CONNECTION_USR.csv'
+WITH DELIMITER ',';
