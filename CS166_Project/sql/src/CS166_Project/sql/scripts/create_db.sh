6f6fb12de514657018c45d9abd2fcc1085f9af1c
Yanjun Guan <67297489+yanjun934523@users.noreply.github.com>
2022-03-18 07:30:35 -0700

    Delete CS166_Project/sql/src/CS166_Project directory

--- CS166_Project/sql/src/CS166_Project/sql/scripts/create_db.sh
+++
@@ -1,6 +0,0 @@
-export DB_NAME=$USER"_DB"
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-psql -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
-psql -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
-psql -p $PGPORT $DB_NAME < $DIR/../src/load_data.sql
