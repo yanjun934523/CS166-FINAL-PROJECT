3edc7caa49278dc56ac571adf3188f011d0cdd9b
Yihua Hao <30879453+yihuaH@users.noreply.github.com>
2022-03-18 21:13:32 -0700

    asd

--- CS166_Project/java/src/ProfNetwork.java
+++ CS166_Project/java/src/ProfNetwork.java
@@ -413,7 +413,7 @@ public class ProfNetwork {
          
          String query = String.format("SELECT U.name, U.email FROM USR U WHERE U.name ='%s'", PeopleName);
          List<List<String>> result = esql.executeQueryAndReturnResult(query);
-         System.out.println(result.toString());
+         System.out.println(result.toString().toString());
       }catch(Exception e){
          System.err.println (e.getMessage ());
          
